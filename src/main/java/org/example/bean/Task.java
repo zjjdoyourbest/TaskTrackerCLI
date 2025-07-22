cdc198251fabf27c75f1adf1d2af8edabd8833ae
zhujunjia <zjjdoyoubest@gmail.com>
2025-07-22 22:43:49 +0900

    v1 测试完成版，有待优化

---
+++ src/main/java/org/example/bean/Task.java
@@ -0,0 +1,73 @@
+package org.example.bean;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task() {
+    }
+
+    public Task(int id, String description, String status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/util/TaskUtil.java
@@ -0,0 +1,78 @@
+package org.example.util;
+
+import org.example.bean.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class TaskUtil {
+
+    private TaskUtil(){
+
+    }
+
+    //检查输入的命令是否满足参数数量要求
+    public static Map<Integer,String> checkLength(String[] params,int length){
+        Integer resultKey=0;
+        String message="";
+        Map<Integer,String> results =new HashMap<>();
+
+
+        //check params length
+        if(params.length != length){
+            resultKey=1;
+            message="The number of input code parameters is incorrect, the correct length is "+length;
+        }
+
+        results.put(resultKey,message);
+        return results;
+    }
+
+    //检查要修改的对象是否存在
+    public static Map<Integer,String> isExist(Map<Integer,Task> arrayList, String id){
+        Integer resultKey=1;
+        String message="id "+ id +" does not exist";
+        Map<Integer,String> results =new HashMap<>();
+        try {
+            int target=Integer.parseInt(id);
+            if(arrayList.containsKey(target)){
+                resultKey = 0;
+                message="";
+            }
+        }catch (Exception e){
+            message="The id must be of numeric type";
+        }
+        results.put(resultKey,message);
+        return results;
+    }
+
+    //打印map
+    public static void printMap(Map<Integer,Task> arrayList,String condition){
+
+        if(!arrayList.isEmpty()) {
+            if(condition.isEmpty()) {
+                System.out.println("|id|-----status-----|---createdAt---|---updatedAt---|------------------description------------------|");
+                for (Map.Entry<Integer, Task> entry : arrayList.entrySet()) {
+                    Task task = entry.getValue();
+                    System.out.println(task.getId() + "  " + task.getStatus() + "    " + task.getCreatedAt() + " " + task.getUpdatedAt() + " " + task.getDescription());
+                }
+            }else {
+                boolean flg=true;
+                for (Map.Entry<Integer, Task> entry : arrayList.entrySet()) {
+                    Task task = entry.getValue();
+                    if(task.getStatus().equals(condition)){
+                        if (flg){
+                            System.out.println("|id|-----status-----|---createdAt---|---updatedAt---|------------------description------------------|");
+                            flg=false;
+                        }
+                        System.out.println(task.getId() + "  " + task.getStatus() + "    " + task.getCreatedAt() + " " + task.getUpdatedAt() + " " + task.getDescription());
+                    }
+                }
+            }
+        }else {
+            System.out.println("there is no data");
+        }
+    }
+
+}
